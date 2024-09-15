d5a75e7ccd2e242903900f66d823e374d851d83f
Ankeet Mourya <mouryaankeet@gmail.com>
2024-09-15 12:59:41 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // or your specified port
+    host: true, // Allow external IP access like 192.168.x.x
+  },
 })
